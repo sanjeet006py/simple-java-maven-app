0e48c72a8f441f910fd76084bef2f2dad4b3afa6
Sanjeet Malhotra <sanjeet.malhotra@sanjeetm-ltm.internal.salesforce.com>
2020-06-09 19:03:49 +0530

    Add existing file

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,17 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            java.util.Random rand = new java.util.Random();
+            rand.setSeed(45);
+            int r = rand.nextInt(2);
+            String str;
+            if(r==0){
+                str = "Hello World!";
+            }
+            else{
+                str = "Hello Word!";
+            }
+            assertEquals(str + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
