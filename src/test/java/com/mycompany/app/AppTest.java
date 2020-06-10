8e966ad1ffc3083d2b0d15c73ecdfbe0bea2c26c
sanjeet.malhotra <thesanjeetmalhotra@gmail.com>
2020-06-10 17:48:14 +0530

    probability of failure is 100%

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -38,7 +38,7 @@ public class AppTest
             //rand.setSeed(45);
             int r = rand.nextInt(100);
             String str;
-            if(r<0){
+            if(r<100){
                 str = "Hello Word!";
             }
             else{
